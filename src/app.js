54029ee2a32bba15d7800aee06583d6700bc9ae2
denis9diaz <150247992+denis9diaz@users.noreply.github.com>
2024-01-26 20:14:55 +0000

    random card terminado

--- src/app.js
+++ src/app.js
@@ -1,11 +1,51 @@
-/* eslint-disable */
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+window.onload = () => {
+  document.querySelector(".card").classList.add(generateRandomSimbolo());
+  document.querySelector(".card").innerHTML = generateRandomNumero();
+};
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  setInterval(() => {
+    generateNewCard();
+  }, 2000);
+
+  document.getElementById("generateNewCard");
+  document.addEventListener("click", generateNewCard);
+};
+
+let generateNewCard = () => {
+  let cardElement = document.querySelector(".card");
+  cardElement.className = "card";
+  cardElement.innerHTML = "card";
+
+  cardElement.classList.add(generateRandomSimbolo());
+  cardElement.innerHTML = generateRandomNumero();
+};
+
+let generateRandomSimbolo = () => {
+  let simbolos = ["heart", "diamond", "club", "spade"];
+  let indexSimbolos = Math.floor(Math.random() * simbolos.length);
+  return simbolos[indexSimbolos];
+};
+
+let generateRandomNumero = () => {
+  let numeros = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let indexNumeros = Math.floor(Math.random() * numeros.length);
+  return numeros[indexNumeros];
 };
